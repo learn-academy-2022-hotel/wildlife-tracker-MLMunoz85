18362c763bf5cdffb22bde3d8b83556be1b1c4b1
MLMunoz85 <matthew.l.munoz@gmail.com>
2023-01-20 16:21:17 -0800

    4/5 of user story 4 complete

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it 'is not valid without a common name' do
+  #   cat = Animal.create scientific_binomial: 'Felis silvestris catus'
+  #   expect(cat.errors[:common_name]).to_not be_empty
+  # end
+  # it 'is not valid without a scientific binomial' do
+  #   cat = Animal.create common_name: 'Cat'
+  #   expect(cat.errors[:scientific_binomial]).to_not be_empty
+  # end
+  # it 'is not valid if the common name exactly matches the scientific binomial' do
+  #   cat = Animal.create common_name: 'Cat', scientific_binomial: 'Cat'
+  #   expect(cat.errors[:common_name] && cat.errors[:scientific_binomial]).to_not be_empty
+  it 'is not valid if the common name and the scientific binomial are not unique' do
+    Animal.create common_name: 'Cat', scientific_binomial: 'Felis silvestris catus'
+    cat = Animal.create common_name: 'Cat', scientific_binomial: 'Felis silvestris catus'
+    expect(cat.errors[:common_name] && cat.errors[:scientific_binomial]).to_not be_empty
+  end
 end
